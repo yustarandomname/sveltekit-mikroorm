6b6db65af8cddfcc44712ba117b2f7aa462b936a
Abel de Bruijn <abeldebruijn@hotmail.com>
2024-05-03 21:08:07 +0200

    chore: Remove commented out code in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-// import viteTs from 'vite-plugin-ts';
 
 export default defineConfig({
 	plugins: [sveltekit()]
